2cb105c39007c7491a836b268df79f84ea0c5292
18014238 <69470946+18014238@users.noreply.github.com>
2022-04-10 15:24:42 +0800

    Fixed x_array bug

--- export_data_from_c/algofunction.py
+++ export_data_from_c/algofunction.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
+import re
 
 
 #def displayGraph():
@@ -13,9 +14,25 @@ import math
 x_array = []
 y_array = []
 
+#Iteration array to store iteration number
+iteration_array = []
+
+#Interval arrays to store interval values
+interval1_array = []
+interval2_array = []
+
+
 with open('result2.txt', 'rt') as myfile:
     for myline in myfile:
-        x = myline[20:38]
-        y = myline[46:55]
-        x_array.append(x)
-        y_array.append(y)
+        x = re.search("x = \[(.*)\] y =", myline)
+        y = re.search("y = (.*) Interval\[0\]", myline)
+        iteration = re.search("Iteration\[(.*)\]: x =", myline)
+        interval1 = re.search("Interval\[0\]: (.*) Interval\[1\]", myline)
+        interval2 = re.search("Interval\[1\]: (.*)", myline)
+        x_array.append(x.group(1))
+        y_array.append(y.group(1))
+        iteration_array.append(iteration.group(1))
+        interval1_array.append(interval1.group(1))
+        interval2_array.append(interval2.group(1))
+
+print(interval1_array)
